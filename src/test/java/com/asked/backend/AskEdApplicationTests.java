d6fb7297cdebd81f54b110b01e721fdbe2944cdd
DHRUV BHOSLE <bdhruv629@outlook.com>
2025-07-02 20:45:42 +0530

    Initial Spring Boot setup for AskEd

---
+++ src/test/java/com/asked/backend/AskEdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asked.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskEdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
